451ed111ee4a0f86e9de500d76f91be0b784ee84
Giriraj Pottekkad <giriraj@geektrust.in>
2022-04-28 11:12:17 +0530

    initial commit

---
+++ geektrust.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+def main():
+    
+    """
+    Sample code to read inputs from the file
+
+    if len(argv) != 2:
+        raise Exception("File path not entered")
+    file_path = argv[1]
+    f = open(file_path, 'r')
+    Lines = f.readlines()
+    """
+    
+if __name__ == "__main__":
+    main()
